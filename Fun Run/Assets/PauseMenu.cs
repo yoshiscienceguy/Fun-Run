2f4271c3e9e887907630f7ab4480269b9e40d7cf
Fernando de Paz <f.depaz@hotmail.com>
2019-05-26 22:38:26 -0700

    Game Done

---
+++ Fun Run/Assets/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool paused;
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            paused = !paused;
+            if (paused)
+            {
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+            }
+            else {
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+            }
+            
+        }
+    }
+}
---
+++ Fun Run/Assets/finishGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class finishGame : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject winScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            other.gameObject.GetComponent<playermovement>().enabled = false;
+            other.gameObject.GetComponentInChildren<AnimationRotation>().enabled = false;
+            other.gameObject.GetComponentInChildren<Animator>().SetFloat("speed",0);
+            other.gameObject.GetComponentInChildren<Animator>().SetTrigger("Done");
+            Player.transform.GetChild(0).transform.localEulerAngles = new Vector3(0, 90, 0);
+            winScreen.SetActive(true);
+        }
+    }
+    public void RestartGame() {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
+
